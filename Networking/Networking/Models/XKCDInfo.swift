effbef590309e1b88962868bfcbe9f7f70bdb0d3
Mikołaj Piechocki <mikolaj.piechocki@elpassion.pl>
2018-10-02 09:32:54 +0200

    wip

---
+++ Networking/Networking/Models/XKCDInfo.swift
@@ -0,0 +1,13 @@
+public struct XKCDInfo: Codable {
+    let month: String
+    let num: Int
+    let link: String
+    let year: String
+//    let news:
+//    let safe_title: String
+    let transcript: String
+    let alt: String
+    let img: String
+    let title: String
+    let day: String
+}
--- Networking/Networking/XKCDApiClient/XKCDApiClient.swift
+++ Networking/Networking/XKCDApiClient/XKCDApiClient.swift
@@ -1,6 +1,21 @@
+public enum Result<Value> {
+    case success(Value)
+    case error
+}
+
 public class XKCDApiClient {
-    let a = "asdf"
-    let b = "asdf"
 
     public init() {}
+
+    public func getCurrent(handler: @escaping (Result<String>) -> Void) {
+        guard let url = URL(string: "https://xkcd.com/info.0.json") else { handler(.error); return }
+        let request = URLRequest(url: url)
+        let session = URLSession(configuration: .default)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            guard error == nil else { handler(.error); return }
+            guard let responseData = data else { handler(.error); return }
+        }
+        task.resume()
+    }
+
 }
