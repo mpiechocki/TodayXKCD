18ceadce93d9e8aef6e1078e9128a45800cb8164
Mikołaj Piechocki <mikolaj.piechocki@elpassion.pl>
2018-10-03 11:54:04 +0200

    Add basic functionality to App's main view controller

--- XKCDToday/ViewController.swift
+++ XKCDToday/ViewController.swift
@@ -1,18 +1,11 @@
-//
-//  ViewController.swift
-//  XKCDToday
-//
-//  Created by Mikolaj Piechocki on 01/10/2018.
-//  Copyright © 2018 Mikolaj Piechocki. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let url = URL(string: "https://xkcd.com")!
+        UIApplication.shared.open(url, options: [:], completionHandler: nil)
     }
 
 }
